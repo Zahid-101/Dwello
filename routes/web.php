09d9fcb8980b7ffd63f4711e8cc62af8e4068a5d
Zahid-101 <zahidwork908@gmail.com>
2025-11-22 12:37:06 +0530

    Updated web file to manage routes

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,20 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PropertyController;
 
+// Home can redirect to properties list (optional)
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('properties.index');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Public: everyone can view the list
+Route::get('/properties', [PropertyController::class, 'index'])
+    ->name('properties.index');
 
+// Protected: only logged-in users can create properties
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+    Route::get('/properties/create', [PropertyController::class, 'create'])
+        ->name('properties.create');
 
-require __DIR__.'/auth.php';
+    Route::post('/properties', [PropertyController::class, 'store'])
+        ->name('properties.store');
+});
